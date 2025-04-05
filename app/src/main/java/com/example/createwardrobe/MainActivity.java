cfa3bb9831251498300d6727e92983aa7fedc52f
Diana <narduak.123@gmail.com>
2025-04-05 18:59:27 +0300

    Фото профілю кругле

--- app/src/main/java/com/example/createwardrobe/MainActivity.java
+++ app/src/main/java/com/example/createwardrobe/MainActivity.java
@@ -8,6 +8,8 @@ import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.graphics.Outline;
+import android.view.ViewOutlineProvider;
 
 import androidx.activity.result.ActivityResult;
 import androidx.activity.result.ActivityResultCallback;
@@ -22,19 +24,15 @@ public class MainActivity extends AppCompatActivity {
     private ImageView imageProfile;
     private Button buttonChoosePhoto;
 
-
     private final ActivityResultLauncher<Intent> pickImageLauncher = registerForActivityResult(
             new ActivityResultContracts.StartActivityForResult(),
             new ActivityResultCallback<ActivityResult>() {
                 @Override
                 public void onActivityResult(ActivityResult result) {
                     if (result.getResultCode() == RESULT_OK && result.getData() != null) {
-
                         Uri selectedImageUri = result.getData().getData();
                         try {
-
                             Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), selectedImageUri);
-
                             imageProfile.setImageBitmap(bitmap);
                         } catch (IOException e) {
                             e.printStackTrace();
@@ -48,10 +46,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
         imageProfile = findViewById(R.id.imageProfile);
         buttonChoosePhoto = findViewById(R.id.buttonChoosePhoto);
 
+
+        imageProfile.setClipToOutline(true);
+        imageProfile.setOutlineProvider(new ViewOutlineProvider() {
+            @Override
+            public void getOutline(View view, Outline outline) {
+
+                int width = view.getWidth();
+                int height = view.getHeight();
+                int radius = Math.min(width, height) / 2;
+                outline.setRoundRect(0, 0, width, height, radius);
+            }
+        });
 
         buttonChoosePhoto.setOnClickListener(new View.OnClickListener() {
             @Override
