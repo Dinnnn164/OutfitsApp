8ef1d3b4f6b039f83fb2ddb5cd469514a79164cb
Diana <narduak.123@gmail.com>
2025-04-13 19:25:15 +0300

    Масиви

--- app/src/main/java/com/example/createwardrobe/ClothingDetailsActivity.java
+++ app/src/main/java/com/example/createwardrobe/ClothingDetailsActivity.java
@@ -1,8 +1,9 @@
 package com.example.createwardrobe;
 
+import android.annotation.SuppressLint;
 import android.graphics.Bitmap;
 import android.os.Bundle;
-import android.widget.EditText;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.Toast;
@@ -12,19 +13,18 @@ import androidx.appcompat.app.AppCompatActivity;
 public class ClothingDetailsActivity extends AppCompatActivity {
 
     ImageView imagePreview;
-    EditText nameInput, sizeInput, brandInput;
-    Spinner seasonSpinner;
 
+    Spinner spinnerTops, spinnerBottoms, spinnerOuterwear,
+            spinnerFullBody, spinnerFootwear, spinnerAccessories, spinnerIntimates;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_clothing_details);
 
         imagePreview = findViewById(R.id.imagePreview);
-        nameInput = findViewById(R.id.nameInput);
-        sizeInput = findViewById(R.id.sizeInput);
-        brandInput = findViewById(R.id.brandInput);
-        seasonSpinner = findViewById(R.id.seasonSpinner);
+
 
         Bitmap imageBitmap = getIntent().getParcelableExtra("imageBitmap");
         if (imageBitmap != null) {
@@ -34,5 +34,30 @@ public class ClothingDetailsActivity extends AppCompatActivity {
         }
 
 
+        spinnerTops = findViewById(R.id.spinnerTops);
+        spinnerBottoms = findViewById(R.id.spinnerBottoms);
+        spinnerOuterwear = findViewById(R.id.spinnerOuterwear);
+        spinnerFullBody = findViewById(R.id.spinnerFullBody);
+        spinnerFootwear = findViewById(R.id.spinnerFootwear);
+        spinnerAccessories = findViewById(R.id.spinnerAccessories);
+        spinnerIntimates = findViewById(R.id.spinnerIntimates);
+
+
+        setupSpinner(spinnerTops, R.array.tops);
+        setupSpinner(spinnerBottoms, R.array.bottoms);
+        setupSpinner(spinnerOuterwear, R.array.outerwear);
+        setupSpinner(spinnerFullBody, R.array.full_body);
+        setupSpinner(spinnerFootwear, R.array.footwear);
+        setupSpinner(spinnerAccessories, R.array.accessories);
+        setupSpinner(spinnerIntimates, R.array.intimates);
+
+    }
+
+    private void setupSpinner(Spinner spinner, int arrayResId) {
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+                this, arrayResId, android.R.layout.simple_spinner_item
+        );
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
     }
 }
